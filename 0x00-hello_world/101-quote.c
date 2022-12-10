1f5a493e99e12ae9bda9dce0bdd309fc46b6c58f
CalistusO <calistus959@gmail.com>
2022-12-10 03:34:46 -0800

    Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful - Dora Korpar, 2015-10-19", followed by a new line, to standard error.
+ * main - prints "and that piece of art is useful - Dora Korpar, 2015-10-19"
  *
  * Return: Always 1 (success)
  */
