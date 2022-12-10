055765d8f4de245835a58552fa5b6a1aa59212b3
CalistusO <calistus959@gmail.com>
2022-12-10 02:47:14 -0800

    Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 /**
@@ -9,6 +10,8 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19, 59);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
 	return (1);
 }
