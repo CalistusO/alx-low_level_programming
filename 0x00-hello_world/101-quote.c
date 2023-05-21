d58005956d8d5cc17278c51ef6b887709c2d0c48
CalistusO <calistus959@gmail.com>
2023-05-21 23:27:41 +0000

    Update all files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 /**
  * main - prints "and that piece of art is useful - Dora Korpar, 2015-10-19"
  *
- * Return: Always 1 (success)
+ * Return: Always 1
  */
 
 int main(void)
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - Entry point
+ *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
