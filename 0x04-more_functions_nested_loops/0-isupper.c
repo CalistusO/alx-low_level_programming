6de49e221ad8fb76d23d1ef24679a906e5dfc6f1
CalistusO <calistus959@gmail.com>
2023-05-30 01:12:40 +0000

    Modify 6-print_line.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,18 @@
 #include "main.h"
-
 /**
- * _isupper - function that checks for uppercase characters
- * @c: int type number
- * Return: 1 if uppercase, else 0
- */
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
-		return (1);
 
+
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+{
 	return (0);
 }
